182eec80bb28e5f0e28f269b78cd8d1371ea6574
Sophie Cowie <sophie_cowie@hotmail.com>
2016-09-19 07:56:18 +0100

    Updating day2_transport notebook

---
+++ scripts/NoCars/plot_barplots_NoCars.py
@@ -0,0 +1,87 @@
+# Make stacked bar plots with various combinations of roads
+# Need to have run make_relative_csv.py before this to make the
+# Exeter_city_only.csv csv
+
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+# read in all the data make a bar plot for each road
+exeter_city = pd.read_csv("../../data/Exeter_city_only.csv", header = 0)
+
+
+#years = [2014,2013,2012,2011,2010,2009,2008,2007,
+#2006,2005,2004,2003,2002,2001,2000]
+
+# Only making the NoCars for 2014 for the presentation
+years = [2014]
+
+
+
+for year in years:
+    # select a year and make a barplot with the distribution of vehicles for each road.
+    exeter_year = exeter_city[exeter_city["AADFYear"] == year]
+    
+    mode = ['PedalCycles', 'Motorcycles', 'BusesCoaches', 'LightGoodsVehicles', 
+           'AllHGVs', 'AllMotorVehicles']
+    
+        
+    # make a dataframe of just the columns we want to stack in the bar plot
+    exeter_year = exeter_year[["Road","PedalCycles","Motorcycles","BusesCoaches",
+                              "LightGoodsVehicles", "AllHGVs"]]
+    
+    # prepare the dataframe for plotting
+    exeter_year.set_index(exeter_year["Road"], inplace = True, drop = True)
+    exeter_year.drop('Road', axis=1, inplace=True)
+    
+    exeter_year.plot.bar(stacked = True)
+    
+    plt.title('Road useage by non-car vehicle type (CPs) %s'%str(year))
+    
+    fig = plt.gcf()
+
+    fig.savefig('../../plots/barplots/NoCars/eachCP_%s.png'%str(year))
+
+    # Perhaps we just want to focus on the smaller roads in Exeter. Lets drop rows with M5 or 
+    exeter_outer = exeter_year[(exeter_year.index == "M5") | (exeter_year.index == "A30")]
+    exeter_inner = exeter_year[(exeter_year.index != "M5") & (exeter_year.index != "A30")]
+    
+    # group by road
+    # Inner
+    group_inner = exeter_inner.groupby(exeter_inner.index).mean()
+    
+    group_inner.plot.bar(stacked = True)
+    
+    plt.title('Inner Road useage by non-car vehicle type %s'%str(year))
+    
+    fig = plt.gcf()
+    fig.savefig('../../plots/barplots/NoCars/inner_barplot%s.png'%str(year))
+    
+    # Outer
+    group_outer = exeter_outer.groupby(exeter_outer.index).mean()
+    
+    group_outer.plot.bar(stacked = True)
+    
+    plt.title('Outer Road useage by non-car vehicle type %s' %str(year))
+    
+    fig = plt.gcf()
+    fig.savefig('../../plots/barplots/NoCars/outer_barplot%s.png'%str(year))
+    
+    # Group all roads
+    group_year = exeter_year.groupby(exeter_year.index).mean()
+    
+    group_year.plot.bar(stacked = True)
+    plt.title('All Road useage by non-car vehicle type %s' %str(year))
+    fig = plt.gcf()
+    fig.savefig('../../plots/barplots/NoCars/each_road_barplot%s.png'%str(year))
+    
+    # Make a plot normalised over each road. 
+   
+    
+    
+    
+    
+    
+    
+    
---
+++ scripts/Outer/plot_relative_maps_outer.py
@@ -0,0 +1,62 @@
+## Script to make annotated maps of Exeter
+## This should be run from exeter_traffic/scripts/
+## make_relative_csv.py should be run before this to create the values to be plotted.
+
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+# Could also exchange the year to make all these plots for each year
+
+# If you don't want to run all the years (will take a while) then alter this
+# list
+#years = ['2014','2013','2012','2011','2010','2009','2008','2007',
+ #'2006','2005','2004','2003','2002','2001','2000']
+years = ['2014']
+
+
+for year in years:
+    # import my normalised coordinate data for just inner data
+    road_coords = pd.read_csv("../../data/road_coords_norm_vals_out%s.csv"%year, header = 0)
+
+    # make a loop over each of the mode types
+    mode = ['PedalCycles', 'Motorcycles', 'CarsTaxis','BusesCoaches', 'LightGoodsVehicles',
+           'AllHGVs','AllMotorVehicles','All']
+    colors = ['b','g','r','indianred','orange','purple','yellow','fuchsia']
+
+    for i,m in enumerate(mode):
+        fig = plt.figure(figsize=(8,8))
+
+        map = Basemap(projection='merc', lat_0 = 57, lon_0 = -135, 
+		      resolution = 'h', area_thresh = 0.2, llcrnrlon=-3.57, llcrnrlat=50.67, 
+                      urcrnrlon=-3.45, urcrnrlat=50.75)
+
+        map.readshapefile('../../data/roads', 'osm_roads')    
+
+        map.drawcoastlines()
+        map.drawcountries()
+        map.fillcontinents(color = 'antiquewhite')
+        map.drawmapboundary()
+
+        for CP in road_coords["CP"].unique():
+
+            lat = road_coords["Latitude"][road_coords["CP"] == CP].tolist()
+            lon = road_coords["Longitude"][road_coords["CP"] == CP].tolist()
+            x,y = map(lon, lat)
+
+            markersize = road_coords[m+"%"][road_coords["CP"] == CP].tolist()[0]
+
+            map.plot(x, y, 'o', color = colors[i], markersize=markersize*120)
+
+        plt.title('Relative AADF for %s %s Outer Roads'%(m,year))    
+
+        fig = plt.gcf()
+        
+        fig.savefig('../../plots/maps/%s/Exeter_%s%s_outer.png'%(year, m,year), dpi=100)
+        
+        plt.clf()
+
+
